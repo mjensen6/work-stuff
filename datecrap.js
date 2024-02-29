226f5405d458f9e3f7ae451e9bde58a78acf3a7c
Matthew Jensen <64800027+mjensen6@users.noreply.github.com>
2024-02-29 04:51:15 -0600

    Update datecrap.js

--- datecrap.js
+++ datecrap.js
@@ -8,4 +8,12 @@ function excelSerialDateToISO(serial) {
   const day = date_info.getUTCDate();
 
   return `${year}-${month.toString().padStart(2, '0')}-${day.toString().padStart(2, '0')}`;
+}function isValidDate(dateString) {
+  const regex = /^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01])$/;
+  return regex.test(dateString);
 }
+
+console.log(isValidDate("2024-02-29")); // true
+console.log(isValidDate("2024-13-01")); // false, because month is invalid
+console.log(isValidDate("2024-02-31")); // false, because day is invalid
+console.log(isValidDate("20240229⬤
